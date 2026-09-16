17417ac807a2cf896cf26958c2aa563716876ea4
angela8973 <2376108@ewhain.net>
2026-09-16 14:31:16 +0900

    Update S03_Assignment.cs

--- Assets/S03/S03_Assignment.cs
+++ Assets/S03/S03_Assignment.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class S03_Assignment : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        // TODO 1: 원하는 다각형의 정점 좌표를 채우세요 (최소 4개)
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 2f, 0f),
+            new Vector3(-2f, 0.5f, 0f),
+            new Vector3(-1.2f, -2f, 0f),
+            new Vector3(1.2f, -2f, 0f),
+            new Vector3(2f, 0.5f, 0f)
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        };
+
+        // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
     }
 }
