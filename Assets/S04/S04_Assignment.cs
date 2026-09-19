1369c8d69dd65d478ab04d5362ba55367fadadbb
angela8973 <2376108@ewhain.net>
2026-09-19 16:31:23 +0900

    Update S04_Assignment.cs

--- Assets/S04/S04_Assignment.cs
+++ Assets/S04/S04_Assignment.cs
@@ -4,23 +4,29 @@ public class S04_Assignment : MonoBehaviour
 {
     void Start()
     {
-        // TODO 1: 원하는 다각형의 정점 좌표를 채우세요 (최소 4개)
         Vector3[] vertices = new Vector3[]
         {
-            new Vector3(0f, 2.5f, 0f),
-            new Vector3(-1.5f, 1f, 0f),
-            new Vector3(-1.5f, -1.5f, 0f),
-            new Vector3(1.5f, -1.5f, 0f),
-            new Vector3(1.5f, 1f, 0f)
+            new Vector3(0f, 1.5f, 0f),
+            new Vector3(0f, -1.5f, 0f),
+            new Vector3(0f, 0f, -1f),
+            new Vector3(1f, 0f, 0f),
+            new Vector3(0f, 0f, 1f)
+            new Vector3(-1f, 0f, 0f)
 
         };
 
         // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
         int[] triangles = new int[]
         {
-            0, 1, 2,
-            0, 2, 3,
-            0, 3, 4
+            0, 3, 2,
+            0, 4, 3,
+            0, 5, 4,
+            0, 2, 5,
+
+            1, 2, 3,
+            1, 3, 4,
+            1, 4, 5,
+            1, 5, 2
         };
 
         Mesh mesh = new Mesh();
